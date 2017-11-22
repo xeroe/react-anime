6f2d962c75b6ea58576b458789b2bda57b0ffa08
Mitchell McCaffrey <mitchemmc@gmail.com>
2017-11-22 13:26:47 +1100

    Removed time based children store

--- packages/react-anime/src/react-anime.js
+++ packages/react-anime/src/react-anime.js
@@ -12,15 +12,6 @@ export class Anime extends Component {
 
     // Current Anime DOM Targets
     this.targets = [];
-
-    // Time based children store
-    let { children } = props;
-    if (!Array.isArray(children)) children = [children];
-    this.children = {
-      cur: children,
-      prev: [],
-      next: []
-    };
   }
 
   componentDidMount() {
@@ -28,35 +19,10 @@ export class Anime extends Component {
   }
 
   componentWillReceiveProps(nextProps) {
-    let { children } = nextProps;
-    let prevChildren = this.props.children;
-
-    if (!Array.isArray(children)) children = [children];
-    if (!Array.isArray(prevChildren)) prevChildren = [prevChildren];
-
-    // Determine diff children
-    let difChildren = children.filter(
-      v => !prevChildren.reduce((prev, cur) => prev || isEqual(v, cur), false)
-    );
-
-    // Determine if children are added/removed
-    let childrenWereRemoved = difChildren.reduce(
-      (prev, cur) => prev || prevChildren.indexOf(cur) > -1,
-      false
-    );
-
-    // Split children to current, old, and new
-    this.children = {
-      cur: children.filter(c => difChildren.indexOf(c) < 0),
-      prev: childrenWereRemoved ? difChildren : this.children.prev,
-      next: !childrenWereRemoved ? difChildren : this.children.next
-    };
-
     this.createAnime(nextProps);
   }
 
   createAnime = (props = this.props) => {
-
     let animeProps = { targets: this.targets, ...props };
 
     anime.remove(this.targets);
@@ -77,12 +43,12 @@ export class Anime extends Component {
    * Render children, and their diffs until promise of anime finishes.
    */
   render() {
-    let { style } = this.props;
-    let { cur, prev, next } = this.children;
+    let { style, children } = this.props;
+    if (!Array.isArray(children)) children = [children];
 
     return (
       <g style={{ ...style }}>
-        {cur.map((child, i) =>
+        {children.map((child, i) =>
           React.cloneElement(child, { key: i, ref: this.addTarget }))}
       </g>
     );
